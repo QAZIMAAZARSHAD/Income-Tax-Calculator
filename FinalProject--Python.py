ed2497e2a7ef46c31881dfda1e3517759f6ab767
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2021-06-10 23:43:28 +0530

    Update Finalproject.py

--- FinalProject--Python.py
+++ FinalProject--Python.py
@@ -253,7 +253,7 @@ def exit_win():
     if (ans):
         root.destroy()  
 
-Button(f3, text="Exit", cursor="hand2", command=end, foreground='white', width=8, font=font1, border=4, bg='#ad0414').place(x=800,
+Button(f3, text="Exit", cursor="hand2", command=exit_win, foreground='white', width=8, font=font1, border=4, bg='#ad0414').place(x=800,
                                                                                                             y=500)
 
 root.mainloop()
